28ded5b24d814f37312103e2bf1375fc7cd758c5
mymyloveyoujung <tram8319@gmail.com>
2023-04-07 08:49:44 +0700

    Trang chu, Empty Cart, Cart

---
+++ Pages/User/Cart.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CheeseBurgerWeb.Pages
+{
+    public class CartModel : PageModel
+    {
+        private readonly ILogger<CartModel> _logger;
+
+        public CartModel(ILogger<CartModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
