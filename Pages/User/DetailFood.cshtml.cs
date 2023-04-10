460fd746980820d099b0506472a6342932c54f36
mymyloveyoujung <tram8319@gmail.com>
2023-04-10 20:17:56 +0700

    tra my

---
+++ Pages/User/DetailFood.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CheeseBurgerWeb.Pages
+{
+    public class DetailFoodModel : PageModel
+    {
+        private readonly ILogger<DetailFoodModel> _logger;
+
+        public DetailFoodModel(ILogger<DetailFoodModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Pages/User/Menu.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CheeseBurgerWeb.Pages
+{
+    public class MenuModel : PageModel
+    {
+        private readonly ILogger<MenuModel> _logger;
+
+        public MenuModel(ILogger<MenuModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
