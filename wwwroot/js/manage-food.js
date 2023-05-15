7b60a6b01039db5b2983eff67b903aa8fe3be289
mymyloveyoujung <tram8319@gmail.com>
2023-05-15 22:27:43 +0700

    Fron-end ThongKe

--- wwwroot/js/manage-food.js
+++ wwwroot/js/manage-food.js
@@ -1,56 +1,3 @@
-// them mon an
-var button = document.querySelector("div.button__add");
-button.addEventListener("click", function () {
-    var modal = document.querySelector('.modal__food-add');
-    modal.classList.add('open');
-});
-// end
-
-// Xem mon an
-var eyeButtons = document.querySelectorAll(".fa-eye");
-
-for (var i = 0; i < eyeButtons.length; i++) {
-    eyeButtons[i].addEventListener("click", function () {
-        var modal = document.querySelector(".modal__food-read");
-        modal.classList.add("open");
-    });
-}
-// End
-
-// Cap nhat mon an
-var pencilButtons = document.querySelectorAll(".fa-pencil-alt");
-
-for (var i = 0; i < pencilButtons.length; i++) {
-    pencilButtons[i].addEventListener("click", function () {
-        var modal = document.querySelector(".modal__ingredient-modify");
-        modal.classList.add("open");
-    });
-}
-// End
-
-// Exit
-var buttonExit = document.querySelectorAll("div.button__exit");
-
-function handleCloseModal() {
-    var modal = this.parentNode.parentNode;
-    modal.classList.remove("open");
-}
-
-for (var i = 0; i < buttonExit.length; i++) {
-    buttonExit[i].addEventListener("click", handleCloseModal);
-}
-
-//OK
-var buttonOK = document.querySelectorAll("button.exit-btn");
-
-function handleOKModal() {
-    var modal = this.parentNode.parentNode.parentNode.parentNode;
-    modal.classList.remove("open");
-}
-for (var i = 0; i < buttonOK.length; i++) {
-    buttonOK[i].addEventListener("click", handleOKModal);
-}
-
 window.onload = function () {
     const urlParams = new URLSearchParams(window.location.search);
     var selectedValueSort = urlParams.get('sortBy');
@@ -113,3 +60,57 @@ form.addEventListener("submit", function (event) {
     }
     form.submit();
 });
+
+// them mon an
+var button = document.querySelector("div.button__add");
+button.addEventListener("click", function () {
+    var modal = document.querySelector('.modal__food-add');
+    modal.classList.add('open');
+});
+// end
+
+// Xem mon an
+var eyeButtons = document.querySelectorAll(".fa-eye");
+
+for (var i = 0; i < eyeButtons.length; i++) {
+    eyeButtons[i].addEventListener("click", function () {
+        var modal = document.querySelector(".modal__food-read");
+        modal.classList.add("open");
+    });
+}
+// End
+
+// Cap nhat mon an
+var pencilButtons = document.querySelectorAll(".fa-pencil-alt");
+
+for (var i = 0; i < pencilButtons.length; i++) {
+    pencilButtons[i].addEventListener("click", function () {
+        var modal = document.querySelector(".modal__ingredient-modify");
+        modal.classList.add("open");
+    });
+}
+// End
+
+// Exit
+var buttonExit = document.querySelectorAll("div.button__exit");
+
+function handleCloseModal() {
+    var modal = this.parentNode.parentNode;
+    modal.classList.remove("open");
+}
+
+for (var i = 0; i < buttonExit.length; i++) {
+    buttonExit[i].addEventListener("click", handleCloseModal);
+}
+
+//OK
+var buttonOK = document.querySelectorAll("button.exit-btn");
+
+function handleOKModal() {
+    var modal = this.parentNode.parentNode.parentNode.parentNode;
+    modal.classList.remove("open");
+}
+for (var i = 0; i < buttonOK.length; i++) {
+    buttonOK[i].addEventListener("click", handleOKModal);
+}
+
